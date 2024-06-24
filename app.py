8f135f3bb40a0b20e362ba1988660584b30f75c4
rahmafaris981 <rahmafaris981@gmail.com>
2024-06-24 22:49:17 +0300

    Ninja Gold

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, redirect, request, session
 import random
 from datetime import datetime
 
+current_time = datetime.now().strftime("%I:%M %p")
+
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
 
@@ -39,10 +41,10 @@ def process_money():
 
     # Create activity entry
     if gold_earned >= 0:
-        activity = f"Earned {gold_earned} gold from the {building}! ({datetime.now()})"
+        activity = f"Earned {gold_earned} gold from the {building}! ({current_time})"
         color = "green"
     else:
-        activity = f"Lost {abs(gold_earned)} gold at the {building}... Ouch! ({datetime.now()})"
+        activity = f"Lost {abs(gold_earned)} gold at the {building}... Ouch! ({current_time})"
         color = "red"
 
     session['activities'].insert(0, {'activity': activity, 'color': color})
